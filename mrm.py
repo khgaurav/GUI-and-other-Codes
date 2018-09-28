e58ff317fa06bd093c4f2207c72eae13e7c36412
Gaurav K H <gaurav.kh1999@gmail.com>
2018-09-28 23:32:48 +0530

    Updated

--- mrm.py
+++ mrm.py
@@ -7,59 +7,55 @@ from time import sleep
 def map1(x,in_min,in_max,out_min,out_max):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 def arm():
-	m1=j.get_button(6)
-	m2=j.get_button(7)
-	m3=j.get_button(8)
-	m4=j.get_button(9)
-	m5=j.get_button(10)
-	m6=j.get_button(11)
-	up=j.get_button(4)
-	down=j.get_button(2)
-	c=0
+	_1u=j.get_button(6)
+	_1d=j.get_button(7)
+	_2u=j.get_button(8)
+	_2d=j.get_button(9)
+	_3u=j.get_button(10)
+	_3d=j.get_button(11)
+	_4u=j.get_button(4)
+	_4d=j.get_button(2)
+	_5u=j.get_button(5)
+	_5d=j.get_button(3)
+	_6u=0
+	_6d=0
 	ser.write('n'.encode())
-	if m1:
-			c=1
-			if up:
-				print('1up')
-				ser.write('A'.encode())
-			if down:
-				print('1down')
-				ser.write('B'.encode())
-	if m2:
-			if up:
-				print('2up')
-				ser.write('C'.encode())
-			if down:
-				print('2down')
-				ser.write('D'.encode())
-	if m3:
-			if up:
-				print('3up')
-				ser.write('E'.encode())
-			if down:
-				print('3down')
-				ser.write('F'.encode())
-	if m4:
-			if up:
-				print('4up')
-				ser.write('G'.encode())
-			if down:
-				print('4down')
-				ser.write('H'.encode())
-	if m5:
-			if up:
-				print('5up')
-				ser.write('I'.encode())
-			if down:
-				print('5down')
-				ser.write('J'.encode())
-	if m6:
-			if up:
-				print('6up')
-				ser.write('K'.encode())
-			if down:
-				print('6down')
-				ser.write('L'.encode())
+	if _1u:
+		print('1up')
+		ser.write('A'.encode())
+	elif _1d:
+		print('1down')
+		ser.write('B'.encode())
+	elif _2u:
+		print('2up')
+		ser.write('C'.encode())
+	elif _2d:
+		print('2down')
+		ser.write('D'.encode())
+	elif _3u:
+		print('3up')
+		ser.write('E'.encode())
+	elif _3d:
+		print('3down')
+		ser.write('F'.encode())
+	elif _4u:
+		print('4up')
+		ser.write('G'.encode())
+	elif _4d:
+		print('4down')
+		ser.write('H'.encode())
+	elif _5u:
+		print('5up')
+		ser.write('I'.encode())
+	elif _5d:
+		print('5down')
+		ser.write('J'.encode())
+	elif _6u:
+		print('6up')
+		ser.write('K'.encode())
+	elif _6d:
+		print('6down')
+		ser.write('L'.encode())
 def motorcode():
 	x1=j.get_axis(0)
 	y1=j.get_axis(1)
