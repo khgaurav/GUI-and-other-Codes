f0e432be8f19be21d3df76daf9ac7ddd37169221
Gaurav K H <gaurav.kh1999@gmail.com>
2018-11-10 21:20:25 +0530

    10/11 9pm

--- gui.py
+++ gui.py
@@ -9,18 +9,69 @@ from time import sleep
 def digitalcam9():
 	os.system("idle-python2.7 -r cam9.py &")
 
+	proc = subprocess.Popen(["xdotool search --onlyvisible --name MRM"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	cmd="xdotool windowmove "+out.strip()+" 0 0 &"
+	os.system(cmd)
+
+	proc = subprocess.Popen(["xdotool search --onlyvisible --name cam9"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	cmd="xdotool windowmove "+out.strip()+" 0 0 &"
+	os.system(cmd)
+	
+	sleep(1)
+	proc = subprocess.Popen(["xdotool search --onlyvisible --sync --name Python\ 2.7.15"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	print out
+	cmd="xdotool windowsize "+out.strip()+" 532 100 && xdotool windowmove "+out.strip()+" 1366 0 &"
+	os.system(cmd)
+	
+	#os.system("xdotool search --onlyvisible --sync --name \"Python\ 2.7.15+\ Shell\" set_window --name \"Motorcode\"")
+	os.system("xdotool windowminimize "+out.strip())
 
 def digitalcam10():
 	os.system("idle-python2.7 -r cam10.py &")
-	proc = subprocess.Popen(["xdotool search --onlyvisible --name frame"], stdout=subprocess.PIPE, shell=True)
+
+	proc = subprocess.Popen(["xdotool search --onlyvisible --name MRM"], stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
-	#print out
-	cmd="xdotool windowmove "+out.strip()+" 1366 768 &"
+	cmd="xdotool windowmove "+out.strip()+" 0 0 &"
 	os.system(cmd)
+
+	proc = subprocess.Popen(["xdotool search --onlyvisible --name cam10"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	cmd="xdotool windowmove "+out.strip()+" 0 0 &"
+	os.system(cmd)
+	sleep(1)
+	proc = subprocess.Popen(["xdotool search --onlyvisible --sync --name Python\ 2.7.15"], stdout=subprocess.PIPE, shell=True)
+
+	(out, err) = proc.communicate()
+	
+	cmd="xdotool windowsize "+out.strip()+" 532 100 && xdotool windowmove "+out.strip()+" 1366 0 &"
+	print cmd
+	os.system(cmd)
+	
+	#os.system("xdotool search --onlyvisible --sync --name \"Python\ 2.7.15+\ Shell\" set_window --name \"Motorcode\"")
+	os.system("xdotool windowminimize "+out.strip())
 
 
 def analogcams():
 	os.system("idle-python2.7 -r analogcams.py &")
+
+
+	proc = subprocess.Popen(["xdotool search --onlyvisible --name MRM"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	cmd="xdotool windowmove "+out.strip()+" 0 0 &"
+	os.system(cmd)
+	
+	#sleep(1)
+	proc = subprocess.Popen(["xdotool search --onlyvisible --sync --name Python\ 2.7.15"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	#print out
+	cmd="xdotool windowsize "+out.strip()+" 532 100 && xdotool windowmove "+out.strip()+" 1366 0 &"
+	os.system(cmd)
+	
+	#os.system("xdotool search --onlyvisible --sync --name \"Python\ 2.7.15+\ Shell\" set_window --name \"Motorcode\"")
+	os.system("xdotool windowminimize "+out.strip())
 
 
 a = Tk()
@@ -32,27 +83,21 @@ Button(a, text="AnalogCams", command = analogcams, bg="white", fg = "black", fon
 
 def motorcode():
 
-	#xdotool windowsize $WINDOW_ID_GOES_HERE $WIDTH $HEIGHT\
 	proc = subprocess.Popen(["xdotool search --onlyvisible --name MRM"], stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
 	cmd="xdotool windowmove "+out.strip()+" 0 0 &"
 	os.system(cmd)
 	os.system("idle-python2.7 -r tcp_send.py &")
-	sleep(1)
-	proc = subprocess.Popen(["xdotool search --onlyvisible --name Python\ 2.7.15"], stdout=subprocess.PIPE, shell=True)
+	#sleep(1)
+	proc = subprocess.Popen(["xdotool search --onlyvisible --sync --name Python\ 2.7.15"], stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
 	#print out
 	cmd="xdotool windowsize "+out.strip()+" 532 100 && xdotool windowmove "+out.strip()+" 1366 0 &"
 	os.system(cmd)
-	os.system("xdotool search --name \"Python 2.7.15+ Shell\" set_window --name \"Motorcode\"")
-
-
-
 	
+	#os.system("xdotool search --onlyvisible --sync --name \"Python\ 2.7.15+\ Shell\" set_window --name \"Motorcode\"")
+	#os.system("xdotool search --onlyvisible --sync --classname --sync --name Python\ 2.7.15 windowminimize")
 
-	
-
-	
 Button(a, text="Motorcode", command = motorcode, bg="white", fg="black",font=("comic_sans",15,"bold")).grid(row = 0,column=2)
 
 def autonomous():
